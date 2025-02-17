e7456268737fdb4465655d02b89bed504a867908
Taliqa <taliqaejaz10@gmail.com>
2025-02-17 19:17:51 +0500

    Fixed Module Federation dependency issue

--- host-app/next.config.ts
+++ host-app/next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "host",
+          remotes: {
+            productsRemote:
+              "productsRemote@http://localhost:3001/remoteEntry.js",
+            basketRemote: "basketRemote@http://localhost:3002/remoteEntry.js",
+          },
+          exposes: {},
+          shared: {
+            react: { singleton: true, requiredVersion: false },
+            "react-dom": { singleton: true, requiredVersion: false },
+          },
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
